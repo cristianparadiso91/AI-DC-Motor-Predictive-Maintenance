854f95f3e4980d5f0f690ee9a396910a785a8c66
cristianparadiso91 <cristianparadiso91@gmail.com>
2026-08-12 14:34:38 -0400

    Add encoder pulse test file structure

---
+++ firmware/ecoder/encoder pulse test.ino
@@ -0,0 +1,14 @@
+AI-DC-Motor-Predictive-Maintenance
+│
+├── firmware
+│   ├── blink-test
+│   ├── motor-control
+│   └── encoder
+│       └── encoder-pulse-test.ino
+│
+├── images
+│   └── testing
+│       └── encoder-pulse-test.jpg
+│
+└── videos
+    └── encoder-pulse-test-working.mp4
